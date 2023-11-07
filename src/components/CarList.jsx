323cded27b21d22a910b6073d44d4b17e2512594
LJBL22 <yourgitemail@example.com>
2023-11-07 15:27:51 +0800

    U332 ~ my own try (not work + fixed

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,5 +1,25 @@
+import { useSelector } from 'react-redux';
+
 const CarList = () => {
-  return <div>CarList</div>;
+  const cars = useSelector((state) => {
+    return state.cars.carList;
+  });
+  // console.log(cars);
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className='panel'>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <ul>{renderedCars}</ul>
+    </div>
+  );
 };
 
 export default CarList;
--- src/store/slices/carsSlice.js
+++ src/store/slices/carsSlice.js
@@ -4,7 +4,7 @@ const carsSlice = createSlice({
   name: "cars",
   initialState: {
     searchTerm: '',
-    carList: []
+    carList: [] // 我自己覺得該改名，下面跟著又忘了代換
   },
   reducers: {
     changeSearchTerm(state, action) {
@@ -13,7 +13,7 @@ const carsSlice = createSlice({
     addCar(state, action) {
       // assumption: 寫下來以記住 payload 的型態
       // action.payload === { name: 'abc', cost: 14000 };
-      state.cars.push({
+      state.carList.push({
         // from the other slice
         name: action.payload.name,
         cost: action.payload.cost,
@@ -23,10 +23,10 @@ const carsSlice = createSlice({
     removeCar(state, action) {
       // assumption: 寫下來以記住 payload 的型態
       // action.payload === { id: xxx };
-      const nextList = state.cars.filter((car) => {
+      const nextList = state.carList.filter((car) => {
         return car.id !== action.payload
       }) // 為什麼是 action.payload?
-      state.cars = nextList;
+      state.carList = nextList;
     },
   }
 }
