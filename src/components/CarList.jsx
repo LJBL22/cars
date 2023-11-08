a64ff3bf114dfb0490bf7a50aa1a3b7f8ae6710d
LJBL22 <yourgitemail@example.com>
2023-11-08 14:58:39 +0800

    ~u342 & ch20 done

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -4,15 +4,7 @@ import { createSelector } from '@reduxjs/toolkit';
 
 const CarList = () => {
   const dispatch = useDispatch();
-  // 與其讓名單跟 search 綁在一起 -> 正確做法：初始顯示的就是 filteredList
-  // // short ver DOESN'T WORK
-  // const memoizedCars = createSelector(({ cars: { searchTerm, carList } }) => {
-  //   return carList.filter((car) =>
-  //     car.name.toLowerCase().includes(searchTerm.toLowerCase())
-  //   );
-  // });
-
-  const memoizedCars = createSelector(
+  const memoizedCarList = createSelector(
     (state) => state.cars.carList,
     (state) => state.cars.searchTerm,
     (carList, searchTerm) => {
@@ -22,41 +14,31 @@ const CarList = () => {
     }
   );
 
-  const carList = useSelector(memoizedCars);
-
-  const reminder = useSelector((state) => {
-    const inputText = state.form.name;
-    const data = state.cars.carList;
-    const matchList = data.filter((car) => {
-      if (car.name.toLowerCase() === inputText.toLowerCase()) {
-        return true;
-      }
-    });
+  const data = useSelector(memoizedCarList);
+  // 曾想合併但似乎無法也無必要，最後拆開寫
+  const name = useSelector((state) => {
+    return state.form.name;
   });
-  //// 對照我的寫法：
-  // const carList = useSelector((state) => {
-  //   const data = state.cars.carList;
-  //   const searchTerm = state.cars.searchTerm;
-  //   const filteredCarList = data.filter((car) => {
-  //     return car.name.includes(searchTerm.toLowerCase());
-  //   });
-  //   return filteredCarList;
-  // });
+
+  //// 原始教案的簡潔寫法（但少了 memoized ）：
+  // const { data, name } = useSelector(
+  //   ({ form, cars: { carList, searchTerm } }) => {
+  //     const filteredCarList = carList.filter((car) =>
+  //       car.name.toLowerCase().includes(searchTerm.toLowerCase())
+  //     );
+  //     return { data: filteredCarList, name: form.name };
+  //   }
+  // );
   const handleCarDelete = (car) => {
     // action.payload === { id: xxx };
     dispatch(removeCar(car.id));
   };
-  const renderedCars = carList.map((car) => {
+  const renderedCars = data.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <div key={car.id} className='panel'>
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
         <p>
-          {true ? (
-            <b>
-              {car.name} - ${car.cost}
-            </b>
-          ) : (
-            `${car.name} - $${car.cost}`
-          )}
+          {car.name} - ${car.cost}
         </p>
         <button
           className='button is-danger'
