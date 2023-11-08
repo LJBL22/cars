9f05f96d965189f6eabcc7d2754c777753ccd587
LJBL22 <yourgitemail@example.com>
2023-11-08 13:32:04 +0800

    ~U341 total cost by filter & reduce

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,20 +1,25 @@
 import { useSelector } from 'react-redux';
 
 const CarValue = () => {
-  const totalCost = useSelector((state) => {
-    const data = state.cars.carList;
-    let total = 0;
-    data.map((car) => {
-      return (total += car.cost);
-    });
-    console.log(total);
-    return total;
-  });
-  return (
-    <div>
-      <h3 className='subtitle is-3'>Total Value: ${totalCost}</h3>
-    </div>
+  const totalCost = useSelector(
+    ({ cars: { searchTerm, carList } }) =>
+      carList
+        .filter((car) =>
+          car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        .reduce((acc, car) => acc + car.cost, 0) // array.prototype.reduce() 將陣列累加縮減成一數/值
+    // let total = 0;
+    // for (let car of data) {
+    //   total += car.cost;
+    // }
+    // return total;
+
+    // my code
+    // // data.map((car) => {
+    // //   return (total += car.cost);
+    // // });
   );
+  return <div className='car-value'>Total Value: ${totalCost}</div>;
 };
 
 export default CarValue;
