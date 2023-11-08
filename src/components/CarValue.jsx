7730454995507bcadc789e8008a12908be6a32c3
LJBL22 <yourgitemail@example.com>
2023-11-08 13:05:54 +0800

    U341 carValue_me try

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,5 +1,20 @@
+import { useSelector } from 'react-redux';
+
 const CarValue = () => {
-  return <div>CarValue</div>;
+  const totalCost = useSelector((state) => {
+    const data = state.cars.carList;
+    let total = 0;
+    data.map((car) => {
+      return (total += car.cost);
+    });
+    console.log(total);
+    return total;
+  });
+  return (
+    <div>
+      <h3 className='subtitle is-3'>Total Value: ${totalCost}</h3>
+    </div>
+  );
 };
 
 export default CarValue;
